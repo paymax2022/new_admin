fbc5489666a44b165960436b546ef5808de7875d
Brainiac <brainiacerudite@gmail.com>
2025-01-09 00:11:48 +0100

    partial template setup

--- src/main.ts
+++ src/main.ts
@@ -29,4 +29,16 @@ app.use(head)
 import appSetting from '@/app-setting'
 appSetting.init()
 
+// tippy tooltips
+import { TippyPlugin } from 'tippy.vue'
+app.use(TippyPlugin)
+
+//input mask
+import { vMaska } from 'maska/vue'
+app.directive('maska', vMaska)
+
+// popper
+import Popper from 'vue3-popper'
+app.component('Popper', Popper)
+
 app.mount('#app')
