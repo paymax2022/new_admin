ee886753d84a3cd8cf55cf10ee34a7b00bb20337
Brainiac <brainiacerudite@gmail.com>
2025-01-13 12:24:19 +0100

    basic voting setup

--- src/router/modules/voting.ts
+++ src/router/modules/voting.ts
@@ -8,6 +8,32 @@ const votingRoutes = [
                 name: 'votingHome',
                 component: () => import('../../views/Voting/VotingHome.vue'),
             },
+            {
+                path: 'contests',
+                // component:
+                children: [
+                    {
+                        path: '',
+                        name: 'votingContestHome',
+                        component: () => import('../../views/Voting/Contest/VotingContestHome.vue'),
+                    },
+                    {
+                        path: 'create',
+                        name: 'votingContestCreate',
+                        component: () => import('../../views/Voting/Contest/VotingContestCreate.vue'),
+                    },
+                    {
+                        path: ':contestId/edit',
+                        name: 'votingContestEdit',
+                        component: () => import('../../views/Voting/Contest/VotingContestEdit.vue'),
+                    },
+                    {
+                        path: ':contestId',
+                        name: 'votingContestDetail',
+                        component: () => import('../../views/Voting/Contest/VotingContestDetail.vue'),
+                    },
+                ],
+            },
         ],
     },
 ];
