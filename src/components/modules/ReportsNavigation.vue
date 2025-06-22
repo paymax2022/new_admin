3399ef6146992fd0b105880e3fc3d6b735c6049b
Abiodun Olorunda <splendidabbey@gmail.com>
2025-06-22 18:15:24 +0100

    2fa and menus fixed

--- src/components/modules/ReportsNavigation.vue
+++ src/components/modules/ReportsNavigation.vue
@@ -8,5 +8,7 @@ import DropdownNav from '@/components/Shared/Nav/DropdownNav.vue';
 
 const links = [
   { route: { name: 'analytics-dashboard' }, displayName: 'Analytics' },
+  { route: { name: 'activity-logs' }, displayName: 'Activity Logs' },
+  { route: { name: 'export-reports' }, displayName: 'Export Reports' },
 ];
 </script>
