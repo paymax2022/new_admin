d84e929cced194f9dc461ace3f052f67286a777f
Hasan Murtaza <hasanmurtaza88@gmail.com>
2025-07-12 22:23:22 +0500

    undo

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: process.env.VITE_API_TARGET || "https://www.spotlightng.com",
+                target: "https://www.spotlightng.com",
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, '/api'),
             },
